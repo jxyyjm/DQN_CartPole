1e41bdc76776ab35d8e3e2d6fd259ecf36a648b4
yujianmin <jxyyjm@gmail.com>
2017-10-11 17:26:36 +0800

    try to upload

---
+++ test.py
@@ -0,0 +1,10 @@
+#!/home/search/anaconda2/bin/python
+import gym
+gym.upload( \
+	'/tmp/cartpole-experiment-1', \
+	writeup='https://gist.github.com/gdb/b6365e79be6052e7531e7ba6ea8caf23', \
+	api_key=' sk_FYp0Gc1dQU69epifs7ZE6w')
+#gym.upload( \
+#	"./gym_results/CartPole-v0-experiment-1/", \
+#	api_key=' sk_FYp0Gc1dQU69epifs7ZE6w')
+#	#api_key="JMXoHnlRtm86Fif6FUw4Qop1DwDkYHy0")
